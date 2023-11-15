10b34fe368e00e1716cbe6c7de5bfb9f44728d3d
test <kidyac1@gmail.com>
2023-11-15 11:40:30 +0900

    챕터 7 초기화블럭

---
+++ src/chapter07/Student.java
@@ -0,0 +1,36 @@
+package chapter07;
+
+public class Student {
+	
+	// 필드
+	String name; // 학생명
+	int grade; // 학년
+	String department; // 학과
+	
+	
+	
+	// 1번 생성자
+	Student() {
+		
+	}
+	
+	Student(String n) {
+		name = n;
+	}
+	
+	// 3번 생성자
+	Student(String n, int g) {
+		name = n;
+		grade = g;
+	}
+	
+	// 4번 생성자
+	Student(String n, int g, String d) {
+		name = n;
+		grade = g;
+		department = d;
+	}
+	
+	
+
+}
