512109127459caae413aff3d311dd1835196037c
test <kidyac1@gmail.com>
2023-11-17 23:01:21 +0900

    챕터 7 완료

---
+++ src/chapter07/test/test2/ClassC.java
@@ -0,0 +1,17 @@
+package chapter07.test.test2;
+
+import chapter07.test.ClassA;
+
+public class ClassC {
+
+	public static void main(String[] args) {
+		
+		ClassA ca = new ClassA();
+		
+		ca.print();
+		
+		// ClassB cb = new ClassB(); 접근제한자에 의해서 에러
+
+	}
+
+}
